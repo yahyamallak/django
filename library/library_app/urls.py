07dc67a12ab0709483efce39f2eb30b094846c27
Yahya mallak <Yahya10mallak@gmail.com>
2022-04-26 02:57:08 +0000

    set up the django database

--- library/library_app/urls.py
+++ library/library_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
+    path('', views.index, name='index'),
+    path('books', views.books, name='books'),
 ]
--- library/library_app/views.py
+++ library/library_app/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def base(request):
+def index(request):
     return render(request, 'pages/index.html')
+
+
+def books(request):
+    return render(request, 'pages/books.html')
