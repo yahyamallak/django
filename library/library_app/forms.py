bebc807bc6ea316b4d7892fb8903fabfa983488c
Yahya mallak <Yahya10mallak@gmail.com>
2022-04-27 05:26:30 +0000

    delete and edit sections

--- library/library_app/forms.py
+++ library/library_app/forms.py
@@ -2,6 +2,16 @@ from tkinter import Widget
 from django import forms
 from .models import Book, Category
 
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
--- library/library_app/views.py
+++ library/library_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import *
-from .forms import BookForm
+from .forms import BookForm, CategoryForm
 
 # Create your views here.
 
@@ -9,12 +10,17 @@ def index(request):
         add_book = BookForm(request.POST, request.FILES)
         if add_book.is_valid():
             add_book.save()
+        
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
 
 
     context = {
         'category': Category.objects.all(),
         'books': Book.objects.all(),
         'form': BookForm(),
+        'formcat': CategoryForm(),
     }
     return render(request, 'pages/index.html', context)
 
@@ -25,3 +31,28 @@ def books(request):
         'books': Book.objects.all(),
     }
     return render(request, 'pages/books.html', context)
+
+
+
+def update(request, id):
+    book_id = Book.objects.get(id=id)
+    if request.method == 'POST':
+        book_save = BookForm(request.POST, request.FILES, instance=book_id)
+        if book_save.is_valid():
+            book_save.save()
+            return redirect('/')
+    else:
+        book_save = BookForm(instance=book_id)
+    
+    context = {
+        'form': book_save,
+    }
+    return render(request, 'pages/update.html' , context)
+
+
+def delete(request, id):
+    book_delete = get_object_or_404(Book, id=id)
+    if request.method == 'POST':
+        book_delete.delete()
+        return redirect('/')
+    return render(request, 'pages/delete.html')
